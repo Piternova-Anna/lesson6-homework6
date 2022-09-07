f48d20ae3876f5c03d7fe93a95fbc4a1c0fe89d6
Anna Piternova <anna-maria999@mail.ru>
2022-09-07 23:27:30 +0300

    hw6

---
+++ вторая задача/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.WriteLine("введите значение b1");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("введите значение k1");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("введите значение b2");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("введите значение k2");
+double k2 = double.Parse(Console.ReadLine());
+
+double x = (b2-b1)/(k1-k2);
+double y = k1 * x + b1;
+
+Console.WriteLine("данные прямые пересекаются в точке с координатами"+"("+ x +";"+ y +")");
---
+++ первая задача/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("Введите количество вводимых значений: ");
+int QuanNums = int.Parse(Console.ReadLine());
+if (QuanNums>=0)
+{
+    int QuanPozitivNums=0;
+ for (int count = 1; count <=QuanNums ;  count++)
+ {
+    Console.Write("Введите "+count+" число: ");
+    int Num = int.Parse(Console.ReadLine());
+    QuanPozitivNums+=Num<0?1:0;
+ }
+
+ Console.WriteLine("отрицательных чисел: " + QuanPozitivNums);
+  
+}
+else
+{
+    Console.WriteLine("количество вводимых чисел должно быть больше 0");
+}
